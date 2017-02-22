3c63b4835b468dc26d348d93979cd8d8dd0c9651
Baqir Farooq <bmconcepts@hotmail.com>
2017-02-22 18:19:47 +0500

    Update Project.php

--- src/Project.php
+++ src/Project.php
@@ -23,7 +23,13 @@ class Project extends AbstractObject {
 
         return $this->client->get("$this->endpoint/$this->id/latestActivity", $args)->response();
     }
-
+    /**
+     * @return mixed
+     */
+    public function updateForce($id, $data)
+    {
+        return $this->client->put("$this->endpoint/".$id, [$this->wrapper => $data])->response();
+    }
     /**
      * Get Companies In Project
      * GET /projects/{project_id}/companies.json
